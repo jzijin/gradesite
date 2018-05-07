5bffafa856d4c09147c3424b201ac5a3d2bb9676
jzijin <jzijin@163.com>
2018-05-07 13:41:07 +0800

    初始化年级网站

---
+++ administrator/logs/error.php
@@ -0,0 +1,15 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-03-15 09:30:22 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-03-15T09:30:22+00:00	INFO 122.96.40.74	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-03-15T09:30:29+00:00	INFO 122.96.40.74	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-03-15T10:13:33+00:00	INFO 122.96.40.74	joomlafailure	用户名和密码不匹配或者你不是网站会员
+2018-04-19T05:11:26+00:00	INFO 112.64.119.73	joomlafailure	用户名和密码不匹配或者你不是网站会员
+2018-04-20T02:18:21+00:00	INFO 117.136.45.103	joomlafailure	用户名和密码不匹配或者你不是网站会员
+2018-04-30T01:02:32+00:00	INFO 114.222.97.248	joomlafailure	用户名和密码不匹配或者你不是网站会员
+2018-05-01T00:58:23+00:00	INFO 219.230.100.101	joomlafailure	用户名和密码不匹配或者你不是网站会员
+2018-05-02T07:01:37+00:00	INFO 219.230.122.63	joomlafailure	用户名和密码不匹配或者你不是网站会员
+2018-05-02T07:09:34+00:00	INFO 219.230.122.63	joomlafailure	不允许使用空密码
